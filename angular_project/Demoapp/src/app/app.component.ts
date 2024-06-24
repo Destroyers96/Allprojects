f23fa2b04fde30757042c2f84094b0005148c7d6
Destroyers <haidar.ameermeea@gmail.com>
2024-06-24 13:14:23 +0400

    Demoapp update

--- angular_project/Demoapp/src/app/app.component.ts
+++ angular_project/Demoapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
